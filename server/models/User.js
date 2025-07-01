5c00f608c3675d68b47365c7054c2112b9df463a
yatfu <55002354+yatfu@users.noreply.github.com>
2025-07-01 13:16:18 -1000

    added user schema, category schema

---
+++ server/models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false
+  }
+});
+
+module.exports = mongoose.model('User', userSchema);
