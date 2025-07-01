91730239d5ca30adf4168f0bb7031f8d793bbcdf
yatfu <55002354+yatfu@users.noreply.github.com>
2025-07-01 01:10:44 -1000

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('API Running'));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
