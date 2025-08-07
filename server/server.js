55436d845ecf6413c5de5f53bafc50c39d1fae2a
yatfu <55002354+yatfu@users.noreply.github.com>
2025-08-07 04:32:46 -1000

    added error handling for .env uri

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-
 
 // INITIALIZE EXPRESS APP
 const app = express();
@@ -15,7 +15,12 @@ app.get('/', (req, res) => res.send('API Running')); // test route
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); // start listening on port PORT
 
 // CONNECT TO MONGODB ATLAS
-mongoose.connect(process.env.MONGO_URI, {
+if (!process.env.MONGO_URI) { // .env error handling
+  console.error("MONGO_URI not defined in .env");
+  process.exit(1);
+}
+
+mongoose.connect(process.env.MONGO_URI, { // connect to atlas
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
